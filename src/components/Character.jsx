e22b0fe379557b9ca2b5b287318da9e8f30337fc
Enrique Romero <94183847+EnriqueRomero06@users.noreply.github.com>
2022-12-12 01:18:14 -0600

    Primera parte

---
+++ src/components/Character.jsx
@@ -0,0 +1,18 @@
+function Character({ character }) {
+	return (
+		<div className="py-5 px-8 relative flex flex-col bg-clip-border bg-slate-700 text-white rounded-2xl shadow-lg shadow-lime-900 ">
+			<h2 className="pb-2 text-center font-semibold text-xl">
+				{character.name}
+			</h2>
+			<img
+				className="rounded-full p-3"
+				src={character.image}
+				alt={character.name}
+			/>
+			<p className="font-semibold">Origen: {character.origin.name}</p>
+			<p className="font-semibold">Ubicacion: {character.location.name}</p>
+		</div>
+	);
+}
+
+export default Character;
---
+++ src/components/CharacterList.jsx
@@ -0,0 +1,61 @@
+import { useEffect, useState } from "react";
+import Character from "./Character";
+
+function Navpage({ page, setPage }) {
+	return (
+		<div className="px-20 py-5 flex grow items-center">
+			<button
+				className=" grow justify-self-end  bg-slate-700 hover:bg-slate-600 text-white font-bold py-0 px-4 border-b-4 border-slate-700 hover:border-slate-600 rounded "
+				onClick={() => {
+					if (page === 1) {
+						page;
+					}
+					setPage(page - 1);
+				}}
+			>
+				prev &#60;-
+			</button>
+			<button
+				className=" grow justify-self-end  bg-slate-700 hover:bg-slate-600 text-white font-bold py-0 px-4 border-b-4 border-slate-700 hover:border-slate-600 rounded "
+				onClick={() => setPage(page + 1)}
+			>
+				nex -&gt;
+			</button>
+		</div>
+	);
+}
+
+function CharacterList() {
+	const [characters, setCharacters] = useState([]);
+	const [loading, setLoading] = useState(true);
+	const [page, setPage] = useState(1);
+	useEffect(() => {
+		async function fetchData() {
+			const response = await fetch(
+				"https://rickandmortyapi.com/api/character?page=" + page,
+			);
+			const data = await response.json();
+			setLoading(false);
+			setCharacters(data.results);
+		}
+		fetchData();
+	}, [page]);
+
+	return (
+		<div>
+			<Navpage page={page} setPage={setPage} />
+			{loading ? (
+				<h1 className="text-xl">Loading</h1>
+			) : (
+				<div className=" p-20 pt-10 grid gap-6 grid-cols-4">
+					{characters.map((character) => {
+						return <Character key={character.id} character={character} />;
+					})}
+				</div>
+			)}
+			<Navpage page={page} setPage={setPage} />
+		</div>
+	);
+}
+
+export default CharacterList;
